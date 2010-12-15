d9808b9a5e6b0599e8ffe55bcaf8b525f5b88d48
Whiteknight <wknight8111@gmail.com>
2010-12-14 21:04:09 -0500

    add a quick top-level exception handler. This exposes an error message where libparrot prints to stderr directly when attempting to load a bytecode file that doesn't exist

--- ParrotTest/Main.cs
+++ ParrotTest/Main.cs
@@ -17,10 +17,13 @@ namespace ParrotTest
 			for (int i = 1; i < args.Length; i++)
 				pbcargs[i - 1] = args[i];
 			Parrot.Parrot parrot = new Parrot.Parrot(exename);
-			Parrot.Parrot_PMC pbc = parrot.LoadBytecodeFile(pbcfile);
-			//Parrot.Parrot_PMC mainargs = pbcargs.ToParrotArrayPMC();
-			Parrot.Parrot_PMC mainargs = parrot.PmcNull;
-			parrot.RunBytecode(pbc, mainargs);
+			try {
+				Parrot.Parrot_PMC pbc = parrot.LoadBytecodeFile(pbcfile);
+				//Parrot.Parrot_PMC mainargs = pbcargs.ToParrotArrayPMC();
+				Parrot.Parrot_PMC mainargs = parrot.PmcNull;
+				parrot.RunBytecode(pbc, mainargs);
+			} catch (Parrot.ParrotException ex) {
+			}
 		}
 	}
 }
