cdc7681525d2a3dcb9656d116dbc9e2d93d7ab22
Whiteknight <wknight8111@gmail.com>
2010-12-17 09:12:52 -0500

    rename Parrot_api_pmc_wrap_string_array

--- src/ParrotExtensionMethods.cs
+++ src/ParrotExtensionMethods.cs
@@ -9,7 +9,7 @@ namespace ParrotSharp
 	public static class ParrotExtensionMethods
 	{
 		[DllImport("parrot")]
-		private static extern int Parrot_api_build_argv_array(IntPtr interp, int argc, IntPtr[] argv, out IntPtr array);
+		private static extern int Parrot_api_pmc_wrap_string_array(IntPtr interp, int argc, IntPtr[] argv, out IntPtr array);
 
 		public static Parrot_PMC ToParrotStringArray(this string[] args, Parrot parrot)
 		{
@@ -17,7 +17,7 @@ namespace ParrotSharp
 			for (int i = 0; i < args.Length; i++) 
 				argv[i] = Marshal.StringToHGlobalAnsi(args[i]);
 			IntPtr pmc = IntPtr.Zero;
-			int result = Parrot_api_build_argv_array(parrot.RawPointer, args.Length, argv, out pmc);
+			int result = Parrot_api_pmc_wrap_string_array(parrot.RawPointer, args.Length, argv, out pmc);
 			if (result != 1)
 				parrot.GetErrorResult();
 			return new Parrot_PMC(parrot, pmc);
