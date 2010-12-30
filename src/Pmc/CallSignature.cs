723d57c4833ae62eaa357961d0ce541485194d13
Léo Grange <grangeleo@gmail.com>
2010-12-30 02:13:37 +0100

    Add an InvokeMethod function to Parrot_PMC, some related changes, and a test function

--- src/Pmc/CallSignature.cs
+++ src/Pmc/CallSignature.cs
@@ -4,11 +4,23 @@ namespace ParrotSharp.Pmc
 {
 	public class CallContext : Parrot_PMC, IParrot_PMC
 	{
+		int ArgNum = 0;
+		
 		public CallContext (Parrot parrot, IntPtr ptr) : base(parrot, ptr) {}
 		
 		public static IPMCFactory<CallContext> GetFactory(Parrot parrot)
 		{
 			return new PMCFactory<CallContext>(parrot, "CallContext");
+		}
+		
+		public Parrot_String Signature {
+			get { return StringValue; }
+			set { StringValue = value; }
+		}
+		
+		public void AddArgument(Parrot_PMC arg) {
+			this[ArgNum] = arg;
+			ArgNum++;
 		}
 	}
 }
