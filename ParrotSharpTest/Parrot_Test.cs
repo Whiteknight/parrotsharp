359b93e5f2002ad2553298973bc1f8b80c3128b6
Whiteknight <wknight8111@gmail.com>
2011-02-07 13:03:22 -0500

    fix some test brokenness. We still can't create multiple top-level interpreters yet.

--- ParrotSharpTest/Parrot_Test.cs
+++ ParrotSharpTest/Parrot_Test.cs
@@ -8,33 +8,11 @@ namespace ParrotSharpTest
 	[TestFixture()]  
 	public class Parrot_Test
 	{
-		
-		private static Parrot ParrotParent = null;
-		
-		private static Parrot ParentInterpreter
-		{
-			get {
-				if (ParrotParent != null)
-					return ParrotParent;
-				else {
-					ParrotParent = new Parrot();
-					return ParrotParent;
-				}
-			}
-		}
-		
-		[Test]
-		public void CreateParrot()
-		{
-			Parrot parrot = new Parrot();
-			Assert.AreNotEqual(IntPtr.Zero, parrot.RawPointer);
-		}
-		
 		[Test]
 		public void CreateParrot_WithParentAndName()
 		{
 			string exename = AppDomain.CurrentDomain.FriendlyName;
-			Parrot parrot = new Parrot(ParentInterpreter, exename);
+			Parrot parrot = new Parrot(TestManager.Interp, exename);
 			Assert.AreNotEqual(IntPtr.Zero, parrot.RawPointer, "Unable to create a new Parrot interpreter");
 		}	
 	}
