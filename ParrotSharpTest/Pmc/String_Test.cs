31f60a1e7ce5a0e544b6b8790fca3587d67a6cd9
Whiteknight <wknight8111@gmail.com>
2011-02-07 14:35:41 -0500

    fixes

--- ParrotSharpTest/Pmc/String_Test.cs
+++ ParrotSharpTest/Pmc/String_Test.cs
@@ -1,11 +1,31 @@
 using System;
+using NUnit.Framework;
+using ParrotSharp;
+using ParrotSharp.Pmc;
+using String = ParrotSharp.Pmc.String;
+
 namespace ParrotSharpTest
 {
+	[TestFixture]
 	public class String_Test
 	{
-		public String_Test ()
+		[Test]
+		public void ParseIntegerValue()
 		{
+			String s = String.GetFactory(TestManager.Interp).Instance();
+			s.StringValue = "127";
+			Integer intpmc = s.ToIntegerPMC(10);
+			Assert.AreEqual(127, intpmc.IntegerValue);
 		}
+		
+		[Test]
+		public void Replace()
+		{
+			String s = String.GetFactory(TestManager.Interp).Instance();
+			s.StringValue = "FooBarBooCar";
+			s.Replace("oo", "X");
+			Assert.AreEqual("FXBarBXCar", s.ToString());
+		}		
 	}
 }
 
--- src/Pmc/IPMCRole_Invocable.cs
+++ src/Pmc/IPMCRole_Invocable.cs
@@ -3,6 +3,7 @@ namespace ParrotSharp
 {
 	public interface IPMCRole_Invocable
 	{
+		IParrot_PMC[] Invoke(IParrot_PMC signature);
 	}
 }
 
